8fa74d0d4f8358c4201f49f91ec4b4916bc2cac2
jeditricks <emmanuel.claudia@gmail.com>
2024-07-22 15:26:27 +0300

    test mock debugger

---
+++ app.js
@@ -0,0 +1,14 @@
+// index.js
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Set a breakpoint on the following line
+const result = add(5, 3) * multiply(2, 4);
+
+console.log(`The result is: ${result}`);
